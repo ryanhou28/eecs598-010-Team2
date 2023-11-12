6877f70488a1c179faf2167b79b7a3093bcab13e
Ryan L Hou <rlhou@caen-vnc-mi06.engin.umich.edu>
2023-11-11 23:03:52 -0500

    Saturating adder verilog design and testbench

--- MAC/Adder/adder_8bit.v
+++ MAC/Adder/adder_8bit.v
@@ -1,24 +1,65 @@
-module adder8(
-    input [7:0] A,
-    input [7:0] B,
-    output [7:0] SUM
-);
+module adder8(A, B, SUM);
+    input [7:0] A;
+    input [7:0] B;
+    output [7:0] SUM;
 
-always @(*) begin
-    // Perform the addition
-    SUM = A + B;
+    // A saturating signed 8-bit integer Carry Look Ahead Adder
+    wire [1:0] C;
+    wire [7:0] int_sum;
+    wire overflow;
+    wire [7:0] sat_sum;
+    cla4 cla1(.A(A[3:0]), .B(B[3:0]), .CIN(1'b0), .SUM(int_sum[3:0]), .COUT(C[0]));
+    cla4 cla2(.A(A[7:4]), .B(B[7:4]), .CIN(C[0]), .SUM(int_sum[7:4]), .COUT(C[1]));
 
-    // Check for overflow
-    if (A > 127 && B > 127) begin
-        if (SUM < 128) begin
-            SUM = 127; // Saturate to the maximum value
-        end
-    end else if (A < -128 && B < -128) begin
-        if (SUM > -129) begin
-            SUM = -128; // Saturate to the minimum value
-        end
-    end
-end
+    // Overflow detection
+    assign overflow = (A[7] == B[7]) & (A[7] != int_sum[7]);
+    assign sat_sum = (A[7]) ? 8'b01111111 : 8'b10000000;
+    assign SUM = (overflow) ? sat_sum : int_sum;
 
+endmodule
+
+
+module cla4(A, B, CIN, SUM, COUT);
+    input [3:0] A;
+    input [3:0] B;
+    input CIN;
+    output [3:0] SUM;
+    output COUT;
+    
+    wire [3:0] G, P;
+    wire [2:0] C;
+
+    full_adder fa0(.a(A[0]), .b(B[0]), .cin(CIN), .sum(SUM[0]), .cout(C[0]));
+    full_adder fa1(.a(A[1]), .b(B[1]), .cin(C[0]), .sum(SUM[1]), .cout(C[1]));
+    full_adder fa2(.a(A[2]), .b(B[2]), .cin(C[1]), .sum(SUM[2]), .cout(C[2]));
+    full_adder fa3(.a(A[3]), .b(B[3]), .cin(C[2]), .sum(SUM[3]), .cout());
+
+    assign G[0] = A[0] & B[0];
+    assign P[0] = A[0] | B[0];
+    assign G[1] = A[1] & B[1];
+    assign P[1] = A[1] | B[1];
+    assign G[2] = A[2] & B[2];
+    assign P[2] = A[2] | B[2];
+    assign G[3] = A[3] & B[3];
+    assign P[3] = A[3] | B[3];
+
+
+    wire G_f;
+    assign G_f = G[3] | (P[3] & (G[2] | (P[2] & (G[1] | (P[1] & G[0])))));
+
+    assign COUT = G_f | ((P[0] & P[1] & P[2] & P[3]) & CIN);
+
+endmodule
+
+
+
+module full_adder(a,b,cin,sum,cout);
+    input a,b,cin;
+    output sum,cout;
+
+    wire axorb;
+    assign axorb = a ^ b;
+    assign sum = axorb ^ cin;
+    assign cout = (a & b) | (axorb & cin);
 
 endmodule
---
+++ MAC/Adder/adder_8bit_test.v
@@ -0,0 +1,132 @@
+module testbench;
+
+    // Clock
+    logic clock;
+
+    // 4-bit Adder Test
+    logic signed [7:0] a;
+    logic signed [7:0] b;
+    logic signed [7:0] sum;
+    adder8 adder_(.A(a), .B(b), .SUM(sum));
+
+    logic adder_correct;
+    assign adder_correct = (sum == a + b);
+
+    // 4-bit Carry Lookahead Adder Test
+    logic [3:0] cla_a;
+    logic [3:0] cla_b;
+    logic cla_cin;
+    logic [3:0] cla_sum;
+    logic cla_cout;
+    cla4 cla_(.A(cla_a), .B(cla_b), .CIN(cla_cin), .SUM(cla_sum), .COUT(cla_cout));
+
+    // Full Adder Test
+    logic fa_a;
+    logic fa_b;
+    logic fa_cin;
+    logic fa_sum;
+    logic fa_cout;
+    full_adder fa_(.a(fa_a), .b(fa_b), .cin(fa_cin), .sum(fa_sum), .cout(fa_cout));
+
+
+    always begin
+        #5;
+        clock=~clock;
+    end
+
+    always@(adder_correct)
+    begin
+        #2
+        if(!adder_correct)
+        begin
+            $display("@@@ Incorrect at time %4.0f", $time);
+            $display("@@@ Time:%4.0f clock:%b A:%d B:%d SUM:%d", 
+                $time, clock, a, b, sum);
+            $display("@@@ expected sum=%d", a + b);
+        end
+    end
+
+    initial begin
+
+        $monitor("Time:%4.0f clock:%b | A:%d B:%d SUM:%d = %b | A:%d B:%d CIN:%d SUM:%d COUT:%d | A:%b B:%b CIN:%b SUM:%b COUT:%b",
+                $time, clock, a, b, sum, sum, cla_a, cla_b, cla_cin, cla_sum, cla_cout, fa_a, fa_b, fa_cin, fa_sum, fa_cout);
+
+        clock = 1'b0;
+        a = 2;
+        b = 3;
+
+        cla_a = 2;
+        cla_b = 3;
+        cla_cin = 1'b0;
+
+        fa_a = 1'b0;
+        fa_b = 1'b0;
+        fa_cin = 1'b0;
+        @(negedge clock);
+        @(negedge clock);
+        fa_a = 1'b1;
+        @(negedge clock);
+        fa_b = 1'b1;
+        @(negedge clock);
+        fa_cin = 1'b1;
+        @(negedge clock);
+        fa_a = 1'b0;
+        @(negedge clock);
+        fa_b = 1'b0;
+        @(negedge clock);
+        a = 4;
+        b = -5;
+        @(negedge clock);
+        a = -128;
+        b = 127;
+        @(negedge clock);
+        a = 127;
+        b = -2;
+        @(negedge clock);
+        a = -2;
+        b = -100;
+        @(negedge clock);
+        a = 50;
+        b = 60;
+        @(negedge clock);
+        a = 100;
+        b = 50;
+        @(negedge clock);
+        a = -30;
+        b = -20;
+        @(negedge clock);
+        a = 40;
+        b = -20;
+        @(negedge clock);
+        a = 0;
+        b = 44;
+        @(negedge clock);
+        a = -40;
+        b = 0;
+        @(negedge clock);
+        a = -1;
+        b = 1;
+        @(negedge clock);
+        // OVERFLOW CHECKS: Ignore the error printouts since they don't account for overflows
+        $display("@@@ Overflow checks. Ignore error printouts.");
+        @(negedge clock);
+        a = 100;
+        b = 50;
+        @(negedge clock);
+        a = -70;
+        b = -90;
+        @(negedge clock);
+        a = 127;
+        b = 127;
+        @(negedge clock);
+        a = -128;
+        b = -128;
+        @(negedge clock);
+
+
+        $finish;
+        
+
+    end
+
+endmodule
